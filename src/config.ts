cf270e23ce6cb9cbf511d28ded2a12b0e1ac5a8c
Tommy Michaud <tm8six@gmail.com>
2026-03-06 11:02:21 -0500

    limits set

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 export const FAUCET_AMOUNTS: Record<string, string> = {
-  SOL_TEST: "0.05",
-  ETH_TEST5: "0.01",
-  ETH_TEST_HOODI: "0.01",
-  BTC_TEST: "0.0001",
+  BTC_TEST: "0.25",
+  ETH_TEST5: "0.2",
+  ETH_TEST_HOODI: "0.2",
+  SOL_TEST: "0.25",
+  USDC_ETH_TEST5_0GER: "5",
+  LINK_ETH_TEST5_BOTX: "1",
 };
